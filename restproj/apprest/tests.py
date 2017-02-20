644ace00d5520f7a52d10811b1c10e13b9fb2b4e
Imrasoft <mutumba.sowed@gmail.com>
2017-02-20 18:57:59 +0300

    Update tests.py

--- restproj/apprest/tests.py
+++ restproj/apprest/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from apprest.models import Group
 
-# Create your tests here.
+class ContactsInGroupTest(TestCase):
+    def creating_groups(self, group_name="test group", number_of_contacts=1):
+        Group.object.create(name=group_name, contacts=number_of_contacts)
+
+    def test_creating_groups(self):
+        group1= self.creating_groups()
+        self.assertEqual()
+
+        
+        
+#STILL WORKING ON THIS        
